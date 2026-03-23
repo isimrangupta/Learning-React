77aac346329aa73bc9cff43a4fc25b620ae3595b
Diksha Gupta <simrandgupta95@gmail.com>
2026-03-23 19:19:50 +0530

    updated code

--- 05Password-Generator/src/main.jsx
+++ 05Password-Generator/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Password from './Password.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Password/>
   </StrictMode>,
 )
